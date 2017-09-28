e7da0affe25f392b3ff8baf35faec1c26f3f098e
anakissler <fhresak@gmail.com>
2017-09-28 12:06:17 -0600

    Updated with Exception handling messages.

--- src/edu/cnm/deepdive/algorithms/Shuffle.java
+++ src/edu/cnm/deepdive/algorithms/Shuffle.java
@@ -8,11 +8,13 @@ import java.util.Random;
 
 /**
  * Implements the Fisher-Yates algorithm
- * @author BIU
+ * @author Ana Kistner
  *
  */
 public class Shuffle {
 
+  private static final String NO_INPUT_MESSAGE = "No size specified; using default value (%d). %n";
+  private static final String BAD_INPUT_MESSAGE = "Could not parse \"%s\" as an int; using default value (%d). %n";
   private static final int DEFAULT_SIZE = 50;
   
   /**
@@ -22,14 +24,36 @@ public class Shuffle {
    * 
    * @param args command line arguments; first value is the array size
    * (default = 50)
-   *    */
+   *    
+   *    
+   */
   public static void main(String[] args) {
-    int size = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_SIZE;
+    int size = DEFAULT_SIZE;
+    try {
+      size = getSize(args);
+    } catch (NumberFormatException ex) {
+      System.out.printf(BAD_INPUT_MESSAGE, args[0], DEFAULT_SIZE);
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      System.out.printf(NO_INPUT_MESSAGE, args[0], DEFAULT_SIZE);
+    }
     int[] values = generate(size);
     System.out.println(Arrays.toString(values));
     shuffle(values);
     System.out.println(Arrays.toString(values));
   }
+  /**
+   * 
+   * @param args
+   * @return
+   * @throws ArrayIndexOutOfBoundsException
+   * @throws NumberFormatException
+   */
+  
+
+  private static int getSize(String[] args) 
+    throws ArrayIndexOutOfBoundsException, NumberFormatException {
+  return Integer.parseInt(args[0]);
+   }
 
   private static int[] generate (int size) {
     int [] data = new int[size];
